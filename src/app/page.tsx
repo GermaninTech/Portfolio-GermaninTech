25e640416226f58dfbd9161c5e1eff8643d3df3b
German Rodriguez Da Silva <144882874+GermaninTech@users.noreply.github.com>
2026-03-06 13:48:35 +0100

    cambios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import Projects from "@/components/Projects";
-import About from "@/components/About";
-import TechStack from "@/components/TechStack";
-import ContactForm from "@/components/ContactForm";
-import Footer from "@/components/Footer";
+import Navbar from "@/app/components/Navbar";
+import Hero from "@/app/components/Hero";
+import Projects from "@/app/components/Projects";
+import About from "@/app/components/About";
+import TechStack from "@/app/components/TechStack";
+import ContactForm from "@/app/components/ContactForm";
+import Footer from "@/app/components/Footer";
 
 export default function Home() {
   return (
